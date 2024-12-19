3ccddf0fbd45b5ab6a453a5900fc47cd95bad6c4
obrusvit <vit.obrusnik@gmail.com>
2024-12-19 16:38:30 +0100

    feat(eckhart): initial commit
    - build
    - directory structure
    - bootloader UI and assets copied from delizia
    - FirmwareUI trait functions are empty
    - Python layout functions are copied from delizia except some of more
    complicated ones which raise NotImplemented for now

---
+++ core/site_scons/ui/ui_eckhart.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from .common import add_font
+
+
+def init_ui(
+    stage: str,
+    config: list[str],
+    defines: list[str | tuple[str, str]],
+    sources: list[str],
+    rust_features: list[str],
+):
+
+    rust_features.append("layout_eckhart")
+
+    if stage == "bootloader":
+        pass
+    if stage in ("prodtest", "bootloader_ci"):
+        add_font("NORMAL", "Font_TTSatoshi_Regular_38", defines, sources)
+        add_font("BOLD", "Font_RobotoMono_Medium_38", defines, sources)
+    if stage == "firmware":
+        rust_features.append("ui_blurring")
+        rust_features.append("ui_jpeg_decoder")
+        rust_features.append("ui_image_buffer")
+        rust_features.append("ui_overlay")
+
+
+def get_ui_layout() -> str:
+    return "UI_LAYOUT_ECKHART"
+
+
+def get_ui_layout_path() -> str:
+    return "trezor/ui/layouts/eckhart/"
