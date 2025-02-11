c66f1e2afa198da7ac4319f047539b33363b64f1
Lukas Bielesch <bielesch.lukas@gmail.com>
2025-02-11 20:48:30 +0100

    feat(eckhart): full-screen share words component

--- core/embed/rust/src/ui/layout_eckhart/component/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/component/mod.rs
@@ -6,11 +6,12 @@ mod header;
 mod hint;
 mod result;
 mod share_words;
+mod share_words_screen;
 mod text_screen;
 mod vertical_menu_page;
 mod welcome_screen;
 
-pub use action_bar::ActionBar;
+pub use action_bar::{ActionBar, ActionBarMsg};
 pub use button::{Button, ButtonMsg, ButtonStyle, ButtonStyleSheet, IconText};
 pub use error::ErrorScreen;
 pub use header::{Header, HeaderMsg};
@@ -18,6 +19,7 @@ pub use hint::Hint;
 pub use result::{ResultFooter, ResultScreen, ResultStyle};
 #[cfg(feature = "translations")]
 pub use share_words::ShareWords;
+pub use share_words_screen::{ShareWordsScreen, ShareWordsScreenMsg};
 pub use text_screen::{AllowedTextContent, TextScreen, TextScreenMsg};
 pub use vertical_menu_page::VerticalMenuPage;
 pub use welcome_screen::WelcomeScreen;
